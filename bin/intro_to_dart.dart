21313944985b11de7d27f8802ed74f21cee0e299
Mohammad-Alhajjar <mohammadalhajjar11@gmail.com>
2026-05-13 17:26:16 +0300

    init commit

---
+++ bin/intro_to_dart.dart
@@ -0,0 +1,39 @@
+void main() {
+  int counter = 10;
+  print(counter);
+
+  double average = 2;
+  print(average);
+
+  bool isFavorite = true;
+  isFavorite = false;
+
+  print(isFavorite);
+
+  String studentName = "Ahmad";
+  print(studentName);
+
+  double sum = counter + 10.5;
+  print(sum);
+
+  //  int sum1 = 10 + "10";
+
+  const double pi = 3.14;
+  // pi = 3.15;
+  print(pi);
+  final int daysCount;
+  daysCount = 7;
+  print(daysCount);
+
+  // daysCount = 8;
+
+  var someThing = "noor";
+  someThing = "Student";
+  print(someThing);
+
+  dynamic anyThing = 10;
+  anyThing = 20.5;
+  anyThing = true;
+  anyThing = "Abd";
+  print(anyThing);
+}
